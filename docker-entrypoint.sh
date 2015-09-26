53d54751ff9a20d0db186c03bd3eb3c3f672804a
Stefan Dimitrov <stefan@dimitrov.li>
2015-09-26 15:42:18 +0000

    Fixes.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,6 @@ echo "yo:$SSH_PASSWORD"|chpasswd
 /etc/init.d/dropbear start
 chown yo:yo -R /home/yo
 cd /home/yo/app
-export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 sudo -u yo ./install.sh
 sudo -u yo flask/bin/python run.py &
 sudo -u yo npm install
