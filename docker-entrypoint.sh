03e282efa87b5816c526b8347fbd8ff055047162
Stefan Dimitrov <stefan@dimitrov.li>
2015-10-23 12:54:12 +0000

    More fixes to entry point.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 chown yo:yo -R /home/yo
 cd /home/yo/app
-sudo -E -u yo ./install.sh
-sudo -E -u yo flask/bin/python run.py &
-sudo -E -u yo npm install
-sudo -E -u yo grunt server
+sudo -H -E -u yo ./install.sh
+sudo -H -E -u yo flask/bin/python run.py &
+sudo -H -E -u yo npm install
+sudo -H -E -u yo grunt server
